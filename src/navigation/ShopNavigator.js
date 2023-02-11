fab1edd95ef6bcf61257c08be3797a25a617a66e
Smael Nicolas <nico.smael@gmail.com>
2023-02-11 13:50:54 -0300

    added tab for shop view

---
+++ src/navigation/ShopNavigator.js
@@ -0,0 +1,36 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+	CategoriesScreen,
+	CategoryProductsScreen,
+	ItemDetailScreen,
+} from "../screens";
+
+const ShopStack = createNativeStackNavigator();
+
+export const ShopNavigator = () => {
+	return (
+		<ShopStack.Navigator initialRoute="Home">
+			<ShopStack.Screen
+				name="Home"
+				options={{
+					title: "TecnoStore",
+				}}
+				component={CategoriesScreen}
+			/>
+			<ShopStack.Screen
+				name="Products"
+				component={CategoryProductsScreen}
+				options={({ route }) => ({
+					title: route.params.name,
+				})}
+			/>
+			<ShopStack.Screen
+				name="Detail"
+				options={({ route }) => ({
+					title: route.params.title,
+				})}
+				component={ItemDetailScreen}
+			/>
+		</ShopStack.Navigator>
+	);
+};
