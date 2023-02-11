d7aa8a1d28e8312a10781ffc3f199d7a0ea419f9
Smael Nicolas <nico.smael@gmail.com>
2023-02-11 13:49:15 -0300

    add dummy data to test application

---
+++ src/data/cart.js
@@ -0,0 +1,13 @@
+export const CART = [
+	{
+		id: 1,
+		stock: 10,
+		title: "Razer Cynosa V2 - US",
+		img: "https://i.ibb.co/0DwNRHg/razer-cynosa-v2-500x500.png",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam blanditiis officia dignissimos! Expedita quod voluptatem molestias id maxime explicabo ipsa.",
+		price: 59.99,
+		category: "keyboard",
+		quantity: 1,
+	},
+];
---
+++ src/data/categories.js
@@ -0,0 +1,6 @@
+export const categoryList = [
+	{ id: "1", name: "keyboard" },
+	{ id: "2", name: "headset" },
+	{ id: "3", name: "mouse" },
+	{ id: "4", name: "pad" },
+];
