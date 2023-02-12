a681e32edad5b95507feac91fa3f845f1b2c0134
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:50:44 -0300

    modify dummy data

--- src/data/cart.js
+++ src/data/cart.js
@@ -10,4 +10,15 @@ export const CART = [
 		category: "keyboard",
 		quantity: 1,
 	},
+	{
+		id: 2,
+		stock: 10,
+		title: "Razer Cynosa V2 - US",
+		img: "https://i.ibb.co/0DwNRHg/razer-cynosa-v2-500x500.png",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam blanditiis officia dignissimos! Expedita quod voluptatem molestias id maxime explicabo ipsa.",
+		price: 59.99,
+		category: "keyboard",
+		quantity: 1,
+	},
 ];
--- src/data/orders.js
+++ src/data/orders.js
@@ -1,1 +1,20 @@
-export const ORDERS = [];
+export const ORDERS = [
+	{
+		id: 1,
+		date: `${new Date().getDate()} - ${new Date().getMonth()} - ${new Date().getFullYear()}`,
+		total: 259.99,
+		items: [
+			{
+				id: 2,
+				stock: 10,
+				title: "Razer Kraken V3 HyperSense",
+				img: "https://i.ibb.co/gSn9vZk/hx-product-headsets-cloud-stinger-core-wireless-71-1-tn.png",
+				description:
+					"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam blanditiis officia dignissimos! Expedita quod voluptatem molestias id maxime explicabo ipsa.",
+				price: 129.99,
+				category: "headset",
+				quantity: 2,
+			},
+		],
+	},
+];
