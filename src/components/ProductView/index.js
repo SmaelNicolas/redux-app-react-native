d70ea956000cf386f6fbe3d39f924b27647edfa3
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:50:35 -0300

    styles product component

--- src/components/ProductView/index.js
+++ src/components/ProductView/index.js
@@ -1,6 +1,27 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
 
-export const ProductDetailView = () => {
-	return <Text>index</Text>;
+export const ProductView = ({ item, onPressed }) => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.containerInfo}>
+				<View>
+					<Text style={styles.text}>{item.title}</Text>
+					<Text style={styles.text}>Price: ${item.price}</Text>
+				</View>
+				<Image style={styles.image} source={{ uri: item.img }} />
+			</View>
+			<View style={styles.containerButtons}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => onPressed(item)}>
+					<Text style={styles.textButton}>Details</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button}>
+					<Text style={styles.textButton}>Buy</Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 };
--- src/components/ProductView/styles.js
+++ src/components/ProductView/styles.js
@@ -1,3 +1,40 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../theme/colors";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+	container: {
+		marginVertical: 10,
+		marginHorizontal: 20,
+		backgroundColor: colors.greyBlue,
+		borderWidth: 1,
+		borderColor: colors.rose,
+		padding: 10,
+	},
+	containerInfo: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	text: { marginVertical: 4, color: colors.rose, fontFamily: "Bitter-Bold" },
+	image: {
+		width: 80,
+		height: 60,
+	},
+	containerButtons: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+		marginVertical: 15,
+	},
+	button: {
+		backgroundColor: colors.darkBlue,
+		paddingHorizontal: 30,
+		paddingVertical: 5,
+		borderRadius: 999,
+		borderWidth: 1,
+		borderColor: colors.white,
+	},
+	textButton: {
+		color: colors.white,
+	},
+});
