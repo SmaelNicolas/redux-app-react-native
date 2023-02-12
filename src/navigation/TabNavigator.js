99851871575538eaa1b168a2a499b1dc64792950
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:51:13 -0300

    add icons to tab navigation

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,5 +1,9 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text, View } from "react-native";
+import { colors } from "../theme/colors";
 import { CartNavigator } from "./CartNavigator";
+import { OrderNavigator } from "./OrderNavigator";
 import { ShopNavigator } from "./ShopNavigator";
 
 const BottomTabs = createBottomTabNavigator();
@@ -11,8 +15,48 @@ export const TabNavigator = () => {
 			screenOptions={{
 				headerShown: false,
 			}}>
-			<BottomTabs.Screen name="ShopTab" component={ShopNavigator} />
-			<BottomTabs.Screen name="CartTab" component={CartNavigator} />
+			<BottomTabs.Screen
+				name="ShopTab"
+				component={ShopNavigator}
+				options={{
+					title: "Shop",
+					tabBarIcon: ({ focused }) => (
+						<Ionicons
+							name={focused ? "home" : "home-outline"}
+							size={26}
+							color={colors.darkBlue}
+						/>
+					),
+				}}
+			/>
+			<BottomTabs.Screen
+				name="CartTab"
+				component={CartNavigator}
+				options={{
+					title: "Cart",
+					tabBarIcon: ({ focused }) => (
+						<Ionicons
+							name={focused ? "cart" : "cart-outline"}
+							size={26}
+							color={colors.darkBlue}
+						/>
+					),
+				}}
+			/>
+			<BottomTabs.Screen
+				name="OrderTab"
+				component={OrderNavigator}
+				options={{
+					title: "Orders",
+					tabBarIcon: ({ focused }) => (
+						<Ionicons
+							name={focused ? "file-tray" : "file-tray-outline"}
+							size={26}
+							color={colors.darkBlue}
+						/>
+					),
+				}}
+			/>
 		</BottomTabs.Navigator>
 	);
 };
