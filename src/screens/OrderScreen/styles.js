08e7deaa59d820371662b2b7518a5652143aa0d7
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:53:14 -0300

    styles for order screen

---
+++ src/screens/OrderScreen/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../theme/colors";
+
+export const styles = StyleSheet.create({
+	container: {
+		marginTop: 45,
+		padding: 10,
+		flex: 1,
+		backgroundColor: colors.darkBlue,
+	},
+	list: {},
+});
