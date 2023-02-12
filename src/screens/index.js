05cb7a913c85138086e93caea6c885e7746689c6
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:52:37 -0300

    export from barrel, all screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,6 @@
-export { CategoriesScreen } from "./CategoriesScreen";
-export { CategoryProductsScreen } from "./CategoryProductsScreen";
-export { ItemDetailScreen } from "./ItemDetailScreen";
+export { CartScreen } from "./CartScreen/CartScreen";
+export { CategoriesScreen } from "./CategoriesScreen/CategoriesScreen";
+export { CategoryProductsScreen } from "./CategoryProductsScreen/CategoryProductsScreen";
+export { ItemDetailScreen } from "./ItemDetailScreen/ItemDetailScreen";
+export { OrderDetailScreen } from "./OrderDetailScreen/OrderDetailScreen";
+export { OrderScreen } from "./OrderScreen/OrderScreen";
