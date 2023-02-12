b23e14182b2105bb602c3b13cbccda9467ff153d
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 13:49:45 -0300

    styled cart item component

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,12 +1,22 @@
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
 
 export const CartItem = ({ handleDelete, item }) => {
 	return (
-		<View>
-			<Text>{item.title}</Text>
-			<TouchableOpacity onPress={() => handleDelete(item.id)}>
-				<Text>Eliminar</Text>
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.title}>{item.title}</Text>
+			<View style={styles.containerPrice}>
+				<Text style={styles.text}>{item.quantity} x </Text>
+				<Text style={styles.text}> ${item.price}</Text>
+			</View>
+			<Text style={styles.total}>
+				Total = {item.price * item.quantity}
+			</Text>
+			<TouchableOpacity
+				style={styles.delete}
+				onPress={() => handleDelete(item.id)}>
+				<Text style={styles.deleteText}>Eliminar</Text>
 			</TouchableOpacity>
-		</View>
+		</SafeAreaView>
 	);
 };
--- src/components/CartItem/styles.js
+++ src/components/CartItem/styles.js
@@ -1,3 +1,33 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../theme/colors";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+	container: {
+		paddingVertical: 5,
+		paddingHorizontal: 10,
+		marginVertical: 5,
+		marginHorizontal: 20,
+		borderWidth: 1,
+		borderColor: colors.rose,
+	},
+	title: { color: colors.rose, fontWeight: "700", fontSize: 20 },
+	containerPrice: {
+		flexDirection: "row",
+	},
+	total: {
+		color: colors.white,
+	},
+	text: { color: colors.rose, fontWeight: "700" },
+	delete: {
+		borderWidth: 1,
+		borderColor: colors.greyBlue,
+		width: 100,
+		alignSelf: "flex-end",
+	},
+	deleteText: {
+		color: colors.rose,
+		fontWeight: "500",
+		textAlign: "center",
+		marginVertical: 5,
+	},
+});
