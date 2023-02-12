1b8fb7d2d01d776be01d122f2c0d23914c1f2d01
Smael Nicolas <nico.smael@gmail.com>
2023-02-12 18:53:54 -0300

    wrapp main component with redux provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { Text } from "react-native";
+import { Provider } from "react-redux";
 import { useFontCustom } from "./hook/useFonts";
 import { AppNavigation } from "./navigation";
+import store from "./store";
 
 export default function App() {
 	const { loaded } = useFontCustom();
 	if (!loaded) return <Text>Loading</Text>;
-	return <AppNavigation />;
+	return (
+		<Provider store={store}>
+			<AppNavigation />
+		</Provider>
+	);
 }
